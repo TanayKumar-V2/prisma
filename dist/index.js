4249e65f3c6238f93a60dcf423b8c99f2cedbe4b
acode-42 <acode4269@gmail.com>
2025-05-10 17:29:06 +0530

    Final change

--- dist/index.js
+++ dist/index.js
@@ -8,18 +8,32 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const client = new client_1.PrismaClient();
-function createUser() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield client.user.create({
-            data: {
-                userName: "Tanay",
-                password: "1234",
-                age: 20,
-            },
-        });
+app.get("/users", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield client.user.findMany();
+    res.json({
+        users,
     });
-}
-createUser();
+}));
+app.get("/todos/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const user = yield client.user.findFirst({
+        where: {
+            id: parseInt(id),
+        },
+        select: {
+            todos: true,
+        },
+    });
+    res.json({
+        user
+    });
+}));
+app.listen(3000);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,50 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
+import bcrypt from "bcrypt";
+
+const app = express();
 
 const client = new PrismaClient();
 
-async function createUser() {
-  await client.user.create({
-    data: {
-      userName: "Tanay",
-      password: "1234",
-      age: 20,
+app.post("/users", async(req, res)=>{
+    const{userName, password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 16);
+    const user = await client.user.create({
+        data: {
+            userName,
+            password: hashedPassword,
+        },
+    });
+
+    const todos=await client.todo.findFirst({
+        select:{
+            user:true
+        }
+    })
+})
+
+app.get("/users", async (req, res) => {
+  const users = await client.user.findMany();
+  res.json({
+    users,
+  });
+});
+
+app.get("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await client.user.findFirst({
+    where: {
+      id: parseInt(id),
+    },
+    select: {
+      todos: true,
     },
   });
-}
 
-createUser()
+  res.json({
+    user
+  })
+
+});
+
+app.listen(3000)
